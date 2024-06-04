da2c25441135261e391d75381c816ef515a091ad
Mario Viloria <211110@ids.upchiapas.edu.mx>
2024-06-03 23:22:05 -0600

    debug{ jenkins try 4 }

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/healt', (req,res) =>{
-    res.send('hello por una vez mas');
+    res.send('hello una ultima vez');
 });
 
 app.listen(port, () => {
